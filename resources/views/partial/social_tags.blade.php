b90cea0277ef378d76bf3850ecc93320ec780caf
Gustavo Ocanto <gustavoocanto@gmail.com>
2015-12-04 13:38:54 -0600

    Fixing Facebook social tags
    - Facebook antvel app configured
    - Product full description
    - Facebook pre-caching images

--- resources/views/partial/social_tags.blade.php
+++ resources/views/partial/social_tags.blade.php
@@ -1,18 +1,22 @@
-<meta name="description" content="{{$description}}">
+<meta name="description" content = "{{ $description }}">
 
-<meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:site" content="@<?=$main_company['twitter']?>">
-<meta name="twitter:title" content="{{$title}}">
-<meta name="twitter:image" content="{{$image}}">
-<meta name="twitter:description" content="{{$description}}">
+<meta name = "twitter:card" content = "summary_large_image">
+<meta name = "twitter:site" content = "@{{ $main_company['twitter'] }}">
+<meta name = "twitter:title" content = "{{ $title }}">
+<meta name = "twitter:image" content = "{{ $image }}">
+<meta name = "twitter:description" content = "{{ $description }}">
 
-<meta property="og:title" content="{{$title}}" />
-<meta property="og:description" content="{{$description}}" />
-<meta property="og:image" content="{{$image}}" >
-<meta property="og:type" content="og:product" />
-<meta property="og:url" content="{{Request::url()}}" />
+<meta property = "og:title" content = "{{ $title }}" />
+<meta property = "og:site_name" content = "{{ $main_company['website_name']}}"/>
+<meta property = "og:description" content = "{{ $description }}" />
+<meta property = "og:image" content = "{{ $image }}" >
+<meta property = "og:type" content = "og:product" />
+<meta property = "og:url" content = "{{ Request::url() }}" />
+<meta property = "fb:app_id" content  =  "{{ env('FB_APP_ID')  }}" />
+<meta property = "og:image:width" content = "450"/>
+<meta property = "og:image:height" content = "298"/>
 
-<meta property="fb:app_id" content="<?=$main_company['facebook_app_id']?>" />
+
 <script type="application/ld+json">
     {
       "@context": "http://schema.org/",
