f546ce32354725de3840d4a8cf4c7b4336ca2fca
Gustavo Ocanto <gustavoocanto@gmail.com>
2016-02-24 22:58:12 -0500

    Applied fixes from StyleCI

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,10 +4,10 @@ namespace app\Http\Controllers;
 
 use App\Eloquent\Collection;
 use App\Eloquent\Model;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
 abstract class Controller extends BaseController
 {
