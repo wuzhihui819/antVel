1dae9ef52acf6aa8fef44cc4bfe4a832c8f857ca
Gustavo Ocanto <gustavoocanto@gmail.com>
2016-03-21 12:59:35 -0500

    Contact page bugs fixed - due to issue #77

--- resources/views/about/contact.blade.php
+++ resources/views/about/contact.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.master')
-@section('page_class')@parent- {{ trans('about.contact_us') }}@stop
+
+@section('page_class')@parent- {{ trans('about.contact_us') }}@endsection
+
 {{-- NAVBAR --}}
 @section('navigation')
-@include('partial.navigation_basic')
-@stop
+  @include('partial.navigation_basic')
+@endsection
+
 {{-- CONTENT --}}
 @section('content')
 <div class="container-fluid">
   <div class="row col-md-12">
     <div class="panel panel-default">
-      
+
       <div class="panel-heading clearfix">
         <h4 class="panel-title pull-left" style="padding-top: 7.5px;"><span class="glyphicon glyphicon-envelope"></span> {{trans('about.contact')}}</h4>
       </div>
@@ -25,15 +28,14 @@
     </div>
   </div>
 </div>
-@stop
 @endsection
 {{-- Pie de pagina --}}
 @section('footer')
 @parent
-@stop
+@endsection
 {{-- Angular --}}
-@section('before.angular') @stop
+@section('before.angular') @endsection
 {{-- Javascript --}}
 @section('scripts')
 @parent
-@stop
+@endsection
--- resources/views/about/map.blade.php
+++ resources/views/about/map.blade.php
@@ -3,7 +3,7 @@
 	  width="100%"
 	  height="300"
 	  frameborder="0" style="border:0; float:left;"
-	  src="https://www.google.com/maps/embed/v1/place?key={{$main_company['google_maps_key_api']}}
+	  src="https://www.google.com/maps/embed/v1/place?key={{ env('GOOGLE_MAP_KEY') }}
 	    &q={{$main_company['address']}},{{$main_company['city']}}+{{$main_company['state']}}" >
 	</iframe>
 </div>
